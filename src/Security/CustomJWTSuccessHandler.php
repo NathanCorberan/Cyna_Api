5356456970bb5a0343a7dfb027dd3c749a05052d
NathanCorberan <nathan.corberan@supdevinci-edu.fr>
2025-05-21 16:47:42 +0200

    ajout de xcartTokken (pas obligatoir)

---
+++ src/Security/CustomJWTSuccessHandler.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace App\Security;
+
+use App\Repository\OrderRepository;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\HttpFoundation\RequestStack;
+use Symfony\Component\Security\Core\User\UserInterface;
+use Lexik\Bundle\JWTAuthenticationBundle\Event\AuthenticationSuccessEvent;
+use Lexik\Bundle\JWTAuthenticationBundle\Events;
+use Symfony\Component\EventDispatcher\EventSubscriberInterface;
+
+class CustomJWTSuccessHandler implements EventSubscriberInterface
+{
+    public function __construct(
+        private RequestStack $requestStack,
+        private OrderRepository $orderRepository,
+        private EntityManagerInterface $entityManager,
+    ) {}
+
+    public static function getSubscribedEvents(): array
+    {
+        return [
+            Events::AUTHENTICATION_SUCCESS => 'onAuthenticationSuccess',
+        ];
+    }
+
+    public function onAuthenticationSuccess(AuthenticationSuccessEvent $event): void
+    {
+        $request = $this->requestStack->getCurrentRequest();
+        $token = $request?->headers->get('X-Cart-Token');
+
+        $user = $event->getUser();
+        if (!$user instanceof UserInterface || !$token) {
+            return;
+        }
+
+        $order = $this->orderRepository->findOneBy(['cartToken' => $token]);
+
+        if ($order && !$order->getUser()) {
+            $order->setUser($user);
+            $this->entityManager->flush();
+        }
+    }
+}
