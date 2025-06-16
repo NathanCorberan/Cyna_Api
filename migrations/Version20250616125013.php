a1f0bdb06d115b221b1251f92c04cc965c521c0a
NathanCorberan <nathan.corberan@supdevinci-edu.fr>
2025-06-16 14:50:57 +0200

    Suppression de tout les migration pour en faire que 1

--- migrations/Version20250616125013.php
+++ migrations/Version20250616125013.php
@@ -10,7 +10,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20250513093745 extends AbstractMigration
+final class Version20250616125013 extends AbstractMigration
 {
     public function getDescription(): string
     {
@@ -20,12 +20,16 @@ final class Version20250513093745 extends AbstractMigration
     public function up(Schema $schema): void
     {
         // this up() migration is auto-generated, please modify it to your needs
-        $this->addSql('ALTER TABLE `order` CHANGE status status ENUM(\'cart\', \'payed\', \'refunded\')');
+        $this->addSql(<<<'SQL'
+            ALTER TABLE `order` CHANGE status status ENUM('cart', 'payed')
+        SQL);
     }
 
     public function down(Schema $schema): void
     {
         // this down() migration is auto-generated, please modify it to your needs
-        $this->addSql('ALTER TABLE `order` CHANGE status status VARCHAR(255) DEFAULT NULL');
+        $this->addSql(<<<'SQL'
+            ALTER TABLE `order` CHANGE status status VARCHAR(255) DEFAULT NULL
+        SQL);
     }
 }
