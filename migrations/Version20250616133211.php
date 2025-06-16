2efd85a5d0fab74931131b25b17b04884812ca33
NathanCorberan <nathan.corberan@supdevinci-edu.fr>
2025-06-16 15:32:39 +0200

    Suppression de tout les migration pour en faire que 1

--- migrations/Version20250616133211.php
+++ migrations/Version20250616133211.php
@@ -10,7 +10,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20250616125817 extends AbstractMigration
+final class Version20250616133211 extends AbstractMigration
 {
     public function getDescription(): string
     {
